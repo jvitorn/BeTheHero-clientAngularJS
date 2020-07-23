f31e7140198ef30c22033b616ff349ca1dba3d69
jvitorn <joaoeuno2011@gmail.com>
2020-07-23 16:27:47 -0300

    mostrando id de usuario para a atualização

--- public/js/controllers/admController.js
+++ public/js/controllers/admController.js
@@ -1,7 +1,10 @@
-angular.module('bethehero').controller('AdmController', function ($scope, $routeParams, verify) {
+angular.module('bethehero').controller('AdmController', function ($scope, $routeParams, recursoOng, getId, verify) {
     verify.teste
-    if ($routeParams.id) {
+    if ($routeParams.id == getId()) {
+        recursoOng.get({ ongId: $routeParams.id }, (ong) => {
+            $scope.usuario = ong.results
 
+        })
 
     }
 
