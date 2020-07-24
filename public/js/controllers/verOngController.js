32d4f3407990840d4684cefea961f9892e55617f
jvitorn <joaoeuno2011@gmail.com>
2020-07-24 11:51:38 -0300

    mostrando nome da on

--- public/js/controllers/verOngController.js
+++ public/js/controllers/verOngController.js
@@ -1,7 +1,11 @@
-angular.module('bethehero').controller('VerOngController', function ($scope, $routeParams, incidentByOng) {
+angular.module('bethehero').controller('VerOngController', function ($scope, $routeParams, incidentByOng, recursoOng) {
     if ($routeParams.id) {
         incidentByOng.get({ ongId: $routeParams.id }, (incident) => {
             $scope.incidents = incident.results
+            recursoOng.get({ ongId: $routeParams.id }, (ong) => {
+                $scope.ong = ong.results.name
+            })
+
         })
     }
 })  
