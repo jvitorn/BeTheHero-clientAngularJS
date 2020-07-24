c45a1a42fdd62db8830253b6c8f439f78edeb203
jvitorn <joaoeuno2011@gmail.com>
2020-07-24 11:41:11 -0300

    mostrando visualizador de ventos da ong

--- public/js/controllers/verOngController.js
+++ public/js/controllers/verOngController.js
@@ -1,3 +1,7 @@
-angular.module('bethehero').controller('VerOngController', function ($scope) {
-
-})
+angular.module('bethehero').controller('VerOngController', function ($scope, $routeParams, incidentByOng) {
+    if ($routeParams.id) {
+        incidentByOng.get({ ongId: $routeParams.id }, (incident) => {
+            $scope.incidents = incident.results
+        })
+    }
+})  
